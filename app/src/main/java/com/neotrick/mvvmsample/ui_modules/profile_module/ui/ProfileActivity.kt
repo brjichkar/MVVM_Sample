29b32704dc1ab6854c9f90dfd71d10052b8b3d5d
brjichkar <bhushanjichkar@gmail.com>
2022-11-29 18:57:37 +0530

    minor UI changes

---
+++ app/src/main/java/com/neotrick/mvvmsample/ui_modules/profile_module/ui/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.neotrick.mvvmsample.ui_modules.profile_module.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neotrick.mvvmsample.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
